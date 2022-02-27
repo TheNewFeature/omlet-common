747cd328fc19b2c1b66dbd7d917e987d5720b0db
rapsealk <piono623@naver.com>
2022-02-27 16:02:59 +0900

    docs: `PEP-257`과 `sphinx` 문법을 따른 주석 작성

--- src/omlet_common/message_queue/__init__.py
+++ src/omlet_common/message_queue/__init__.py
@@ -4,8 +4,17 @@ import pika
 
 
 class RabbitQueue:
+    """`RabbitMQ`를 사용하기 위한 클래스"""
     def __init__(self, host: str = '127.0.0.1', port: int = 5672,
                  vhost: str = '/', username: str = 'guest', password: str = 'guest'):
+        """
+        Args:
+            :param str host: 메시지 큐 서버의 주소
+            :param int port: 메시지 큐 서버의 포트 번호
+            :param str vhost: `Virtual Host`의 이름 (https://www.rabbitmq.com/vhosts.html)
+            :param str username: 메시지 큐 서버의 사용자 이름
+            :param str password: 메시지 큐 서버의 사용자 비밀번호
+        """
         self._connection = pika.BlockingConnection(
             pika.ConnectionParameters(host=host, port=port, virtual_host=vhost,
                                       credentials=pika.PlainCredentials(username, password))
@@ -18,6 +27,13 @@ class RabbitQueue:
         self._channel = channel
 
     def publish(self, message: Union[bytes, str], exchange: str = '', routing_key: str = ''):
+        """메시지 큐에 메시지를 발송합니다.
+
+        Args:
+            :param Union[bytes, str] message: 메시지 큐에 전달할 메시지 내용
+            :param str exchange: ... (https://www.rabbitmq.com/tutorials/tutorial-three-python.html)
+            :param str routing_key: ... (https://www.rabbitmq.com/tutorials/tutorial-four-python.html)
+        """
         if type(message) is str:
             message = message.encode('utf-8')
         self._channel.basic_publish(exchange=exchange, routing_key=routing_key, body=message,
@@ -25,6 +41,12 @@ class RabbitQueue:
 
     def subscribe(self, queue: str,
                   on_message_callback: Callable[[pika.adapters.blocking_connection.BlockingChannel, str, str, bytes], None]):
+        """메시지를 수신하기 위한 큐를 생성하고 콜백 함수를 등록합니다.
+
+        Args:
+            :param str queue: 수신할 큐 이름
+            :paral Callable on_message_callback: 메시지가 도착했을 때 수행할 동작이 정의된 콜백 함수
+        """
         self._channel.queue_declare(queue, durable=True, auto_delete=True)
         self._channel.basic_consume(queue, on_message_callback=on_message_callback)
         try:
@@ -34,5 +56,6 @@ class RabbitQueue:
         self.close()
 
     def close(self):
+        """`Connection`을 종료합니다."""
         if self._connection.is_open:
             self._connection.close()
